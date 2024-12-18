60a7d5eba8232e0a110916da970f1faea3b74cef
Fahed Khan <127182880+12fahed@users.noreply.github.com>
2024-12-18 19:00:52 +0530

    Update page.tsx

--- src/app/landing/@instructor/uploadAssignment/page.tsx
+++ src/app/landing/@instructor/uploadAssignment/page.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-import CreateAssignmentDialog from "@/components/CreateAssignmentDialog"
 import InstructorTable from "@/components/InstructorTable"
 
 export default function Assignmnet(){
 
   return(
     <div>
-      {/* <CreateAssignmentDialog /> */}
       <InstructorTable />
     </div>
   )
